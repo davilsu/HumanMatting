79530036fd0ea7fef23d92435b25879265ef7778
Haowei Wen <yushijinhun@gmail.com>
2022-05-29 22:56:03 +0800

    add toolbar to camera activity

--- app/src/main/java/com/davilsu/peoplematting/CameraActivity.java
+++ app/src/main/java/com/davilsu/peoplematting/CameraActivity.java
@@ -1,19 +1,59 @@
 package com.davilsu.peoplematting;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.otaliastudios.cameraview.CameraView;
+import com.otaliastudios.cameraview.controls.Facing;
 
 public class CameraActivity extends AppCompatActivity {
+
+    private CameraView cameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
-        CameraView camera = findViewById(R.id.camera);
-        camera.setLifecycleOwner(this);
+        setTitle("人像抠图");
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        setSupportActionBar(findViewById(R.id.toolbar));
+
+        cameraView = findViewById(R.id.camera);
+        cameraView.setLifecycleOwner(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_camera, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.switch_camera:
+                switch (cameraView.getFacing()) {
+                    case FRONT:
+                        cameraView.setFacing(Facing.BACK);
+                        break;
+                    case BACK:
+                        cameraView.setFacing(Facing.FRONT);
+                        break;
+                }
+                return true;
+            case R.id.settings:
+                // TODO settings
+                return true;
+            default:
+                return false;
+        }
     }
 }
