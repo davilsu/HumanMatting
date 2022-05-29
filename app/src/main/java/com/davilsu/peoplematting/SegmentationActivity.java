2aea0547ee35575ea74d8918bc40281aaa5f9f64
Haowei Wen <yushijinhun@gmail.com>
2022-05-30 05:03:48 +0800

    add save image

--- app/src/main/java/com/davilsu/peoplematting/SegmentationActivity.java
+++ app/src/main/java/com/davilsu/peoplematting/SegmentationActivity.java
@@ -6,13 +6,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.exifinterface.media.ExifInterface;
 import androidx.preference.PreferenceManager;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,8 +25,12 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.time.LocalDateTime;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -32,6 +41,8 @@ public class SegmentationActivity extends AppCompatActivity {
     private static final Executor executor = Executors.newSingleThreadExecutor();
 
     private ImageView imageView;
+    @Nullable
+    private Bitmap bitmapAlpha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,17 +77,18 @@ public class SegmentationActivity extends AppCompatActivity {
         Bitmap outputBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
         MattingNetwork network = new MattingNetwork();
         if (!network.Init(getAssets(), prefMode)) {
-            runOnUiThread(() -> Toast.makeText(getApplicationContext(), "Device does not support big.LITTLE architecture", Toast.LENGTH_LONG).show());
+            runOnUiThread(() -> Toast.makeText(this, "Device does not support big.LITTLE architecture", Toast.LENGTH_LONG).show());
         }
         int ret = network.Process(getAssets(), outputBitmap, modelIndex, fp16Quantization, int8Quantization, useGpu);
         if (ret < 0) {
-            runOnUiThread(() -> Toast.makeText(getApplicationContext(), "Inference Error", Toast.LENGTH_LONG).show());
+            runOnUiThread(() -> Toast.makeText(this, "Inference Error", Toast.LENGTH_LONG).show());
             return;
         }
         double elapsedTime = ret / 100.0;
-        runOnUiThread(() -> Toast.makeText(getApplicationContext(), String.format("Inference time: %.2fms", elapsedTime), Toast.LENGTH_SHORT).show());
+        runOnUiThread(() -> Toast.makeText(this, String.format("Inference time: %.2fms", elapsedTime), Toast.LENGTH_SHORT).show());
 
         runOnUiThread(() -> {
+            bitmapAlpha = outputBitmap;
             ProgressBar progressbar = findViewById(R.id.progress_bar);
             progressbar.setVisibility(View.GONE);
             imageView.setImageBitmap(outputBitmap);
@@ -125,8 +137,33 @@ public class SegmentationActivity extends AppCompatActivity {
         // TODO
     }
 
+    private OutputStream saveToGallery(String filename) throws FileNotFoundException {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            ContentResolver resolver = getContentResolver();
+            ContentValues values = new ContentValues();
+            values.put(MediaStore.MediaColumns.DISPLAY_NAME, filename);
+            values.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg");
+            values.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES);
+            Uri imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            return resolver.openOutputStream(imageUri);
+        } else {
+            File imagesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            return new FileOutputStream(new File(imagesDir, filename));
+        }
+    }
+
     public void savePhoto() {
-        // TODO
+        if (bitmapAlpha == null) {
+            return;
+        }
+
+        String filename = LocalDateTime.now().toString() + ".jpg";
+        try (OutputStream outputStream = saveToGallery(filename)) {
+            bitmapAlpha.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Toast.makeText(this, "Saved to " + filename, Toast.LENGTH_SHORT).show();
     }
 
     private Bitmap decodeUri(Uri selectedImage) throws FileNotFoundException {
