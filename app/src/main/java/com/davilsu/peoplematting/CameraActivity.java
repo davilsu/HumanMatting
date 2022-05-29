2a3112e0e39c797423610107629455a9576d6708
Haowei Wen <yushijinhun@gmail.com>
2022-05-30 00:41:09 +0800

    preferences done

--- app/src/main/java/com/davilsu/peoplematting/CameraActivity.java
+++ app/src/main/java/com/davilsu/peoplematting/CameraActivity.java
@@ -2,8 +2,10 @@ package com.davilsu.peoplematting;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -27,7 +29,10 @@ public class CameraActivity extends AppCompatActivity {
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         setSupportActionBar(findViewById(R.id.toolbar));
 
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
         cameraView = findViewById(R.id.camera);
+        cameraView.setFacing(Facing.valueOf(preferences.getString("default_camera_face", "FRONT")));
         cameraView.setLifecycleOwner(this);
     }
 
@@ -41,20 +46,28 @@ public class CameraActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.switch_camera:
-                switch (cameraView.getFacing()) {
-                    case FRONT:
-                        cameraView.setFacing(Facing.BACK);
-                        break;
-                    case BACK:
-                        cameraView.setFacing(Facing.FRONT);
-                        break;
-                }
+                switchCamera();
                 return true;
             case R.id.preferences:
-                startActivity(new Intent(this,PreferencesActivity.class));
+                openPreferences();
                 return true;
             default:
-                return false;
+                return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void switchCamera() {
+        switch (cameraView.getFacing()) {
+            case FRONT:
+                cameraView.setFacing(Facing.BACK);
+                break;
+            case BACK:
+                cameraView.setFacing(Facing.FRONT);
+                break;
+        }
+    }
+
+    public void openPreferences() {
+        startActivity(new Intent(this, PreferencesActivity.class));
     }
 }
